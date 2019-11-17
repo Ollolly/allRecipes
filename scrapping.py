48bbe57406c0107dbc97e440689a59210a846bfc
olga <hazin.olga@gmailcom>
2019-11-18 00:32:42 +0200

    script to extract all recipes by subcategory (without scrolling down)

---
+++ scrapping.py
@@ -0,0 +1,73 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+URL = 'https://www.allrecipes.com/'
+SCROLL_DOWN = 4
+CATEGORY = '#insideScroll > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1)'
+SUBCATEGORY = ['Butter Cookies']
+
+
+def scroll_down(browser, number_of_scroll_downs):
+    """ scrolls web page down """
+    body = browser.find_element_by_tag_name("body")
+    while number_of_scroll_downs >= 0:
+        body.send_keys(Keys.PAGE_DOWN)
+        number_of_scroll_downs -= 1
+    return browser
+
+
+def get_category_link(url):
+    """ returns a link to a category """
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+    href = soup.select(CATEGORY)[0]['href']
+    return href
+
+
+def get_subcategory_links(url):
+    """ returns a list of subcategory links """
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+    base = soup.find('div', id="insideScroll")
+    sub_category_list = []
+    for link in BeautifulSoup(str(base), 'lxml').findAll('a'):
+        if link.span.text in SUBCATEGORY:
+            sub_category_list.append(link['href'])
+    return sub_category_list
+
+
+def get_recipe_links(url):
+    """ returns links to all recipes on webpage """
+    # browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+    # browser.get(url)
+    # time.sleep(1)
+    # browser = scroll_down(browser, SCROLL_DOWN)
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+    # find all recipes and extract their links
+    links = soup.select('article.fixed-recipe-card div.grid-card-image-container a')
+    links = [link['href'] for link in links]
+    return links
+
+
+def main():
+    link = get_category_link(URL)
+    subcategory = get_subcategory_links(link)
+    recipes = []
+    for cat in subcategory:
+        recipes.append(get_recipe_links(cat))
+
+    print(recipes)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
