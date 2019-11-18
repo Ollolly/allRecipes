ea5417abad098a640e583c14a79c427678bbbf4a
Ravit Mesika <ravit2244@gmail.com>
2019-11-18 11:52:14 +0200

    containing the functions get_recipes_details which extract data from list of urls, and the function write_data_to_csv, which writes csv file from the dictionary recipes_data

--- get_recepie_details.py
+++ get_recepie_details.py
@@ -7,132 +7,103 @@ from collections import defaultdict
 import csv
 import re
 
+CATEGORIES = ['url', 'author', 'review', 'summary', 'name', 'rating', 'image', 'prep_time',
+              'calories', 'pieces']
 
-path = r'C:\Users\ravit\Documents\ITC\chromedriver.exe'
-browser = webdriver.Chrome(path)
-browser.maximize_window() #//For maximizing window
-browser.implicitly_wait(20) #//gives an implicit wait for 20 seconds
-browser.get('https://www.allrecipes.com/')
-cookies = browser.find_element_by_xpath('//*[@id="insideScroll"]/ul/li[3]/a/img')
-cookies.click()
-butter = browser.find_element_by_xpath('//*[@id="insideScroll"]/ul/li[4]/a/img')
-butter.click()
-most_made_today = browser.find_element_by_class_name('list-recipes__carousel')
-url_most_made_today=most_made_today.get_attribute('innerHTML')
-print(url_most_made_today)
-with open(r'url_most_made.html','w') as f:
-    f.write(url_most_made_today)
 
-with open(r'url_most_made.html','r') as f:
-    page = f.read()
+def get_recipes_details(urls):
+    """ Extract details for each link from the variable 'urls' """
+    recipes_data = defaultdict(dict)
+    for i,url in enumerate(urls):
 
-soup = bs4.BeautifulSoup(page,"lxml")
-# soup=soup.prettify()
+        # extracting the url data
+        try:
+            page = requests.get(url)
+            soup = bs4.BeautifulSoup(page.content, "lxml")
+            recipes_data[i]['url'] = url
+        except FileNotFound or TypeError:
+            recipes_data[i]['url'] = None
 
-recipes=defaultdict(list)
-recipe_data=soup.find_all('li',class_='list-recipes__recipe')
+        # extracting the author data
+        try:
+            s_author = soup.select('span[itemprop="author"]')[0]
+            author = s_author.text
+            recipes_data[i]['author'] = author
+        except TypeError:
+            recipes_data[i]['author'] = None
 
-for i, recipe in enumerate(recipe_data):
-    try:
-        url_recipe = recipe.a['href'].split()[0]
-        recipes[i] += [url_recipe]
-    except TypeError:
-        continue
-recipes=dict(recipes)
+        # extracting number of reviews
+        try:
+            s_review = soup.select('span[class="review-count"]')[0]
+            review = s_review.text
+            recipes_data[i]['review'] = review
+        except TypeError:
+            recipes_data[i]['review'] = None
 
-csv_recipes_today = open(r'most_made_today_links.csv', 'w' ,newline='')
-csv_writer= csv.writer(csv_recipes_today)
-headers = ['Source']
-csv_writer.writerow(headers)
-for row in recipes.values():
-    csv_writer.writerow(row)
-csv_recipes_today.close()
+        # extracting the summary data
+        try:
+            s_summary= soup.select('div[itemprop="description"]')[0]
+            summary = s_summary.text
+            recipes_data[i]['summary'] = summary
+        except TypeError:
+            recipes_data[i]['summary'] = None
 
-with open(r'most_made_today_links.csv','r') as f:
-    page = f.readlines()
-urls=page[1:]
-print(urls)
+        # extracting the recipe name
+        try:
+            s_name= soup.select('h1[class="recipe-summary__h1"]')[0]
+            name = s_name.text
+            recipes_data[i]['name'] = name
+        except TypeError:
+            recipes_data[i]['name'] = None
 
-recipes_data = defaultdict(dict)
-categories = ['url', 'author', 'review', 'summary', 'name', 'rating', 'image', 'prep_time', 'calories', 'pieces']
-for i,url in enumerate(urls):
-    try:
-        file=requests.get(url)
-        soup = bs4.BeautifulSoup(file.content,"lxml")
-        recipes_data[i]['url'] = url
-    except FileNotFound or TypeError:
-        recipes_data[i]['url'] = None
+        # extracting the recipe rating
+        try:
+            s_rating = soup.find('div',class_="rating-stars")
+            rating = s_rating['data-ratingstars']
+            recipes_data[i]['rating'] = rating
+        except TypeError:
+            recipes_data[i]['rating'] = None
 
-    try:
-        s_author = soup.select('span[itemprop="author"]')[0]
-        author = s_author.text
-        recipes_data[i]['author'] = author
-    except TypeError:
-        recipes_data[i]['author'] = None
+        # extracting the recipe image
+        try:
+            s_image = soup.find('img',class_="rec-photo")
+            image = s_image['src']
+            recipes_data[i]['image'] = image
+        except TypeError:
+            recipes_data[i]['image'] = None
 
-    try:
-        s_review = soup.select('span[class="review-count"]')[0]
-        review = s_review.text
-        recipes_data[i]['review'] = review
-    except TypeError:
-        recipes_data[i]['review'] = None
+        # extracting the preparation time
+        try:
+            s_prep_time= soup.select('span[aria-label="Ready in 50 Minutes"]')[0]
+            prep_time=s_prep_time.text
+            recipes_data[i]['prep_time'] = prep_time
+        except:
+            recipes_data[i]['prep_time'] = None
 
-    try:
-        s_summary= soup.select('div[itemprop="description"]')[0]
-        summary = s_summary.text
-        recipes_data[i]['summary'] = summary
-    except TypeError:
-        recipes_data[i]['summary'] = None
+        # extractiong the calories data
+        try:
+            s_calorie= soup.select('span[class="calorie-count"]')[0]
+            calorie=s_calorie.text
+            recipes_data[i]['calories'] = calorie
+        except TypeError:
+            recipes_data[i]['calories'] = None
 
-    try:
-        s_name= soup.select('h1[class="recipe-summary__h1"]')[0]
-        name = s_name.text
-        recipes_data[i]['name'] = name
-    except TypeError:
-        recipes_data[i]['name'] = None
+        # extracting the cookie quantity
+        try:
+            s_pieces= soup.find('span',class_="ng-binding")[0]
+            pieces=s_pieces.text
+            recipes_data[i]['pieces'] = pieces
+        except TypeError:
+            recipes_data[i]['pieces'] = None
 
-    try:
-        s_rating = soup.find('div',class_="rating-stars")
-        rating = s_rating['data-ratingstars']
-        recipes_data[i]['rating'] = rating
-    except TypeError:
-        recipes_data[i]['rating'] = None
+    return recipes_data
 
-    try:
-        s_image = soup.find('img',class_="rec-photo")
-        image = s_image['src']
-        recipes_data[i]['image'] = image
-    except TypeError:
-        recipes_data[i]['image'] = None
 
-    try:
-        s_prep_time= soup.select('span[aria-label="Ready in 50 Minutes"]')[0]
-        prep_time=s_prep_time.text
-        recipes_data[i]['prep_time'] = prep_time
-    except:
-        recipes_data[i]['prep_time'] = None
-
-    try:
-        s_calorie= soup.select('span[class="calorie-count"]')[0]
-        calorie=s_calorie.text
-        recipes_data[i]['calories'] = calorie
-    except TypeError:
-        recipes_data[i]['calories'] = None
-
-    try:
-        s_pieces= soup.find('span',class_="ng-binding")[0]
-        pieces=s_pieces.text
-        recipes_data[i]['pieces'] = pieces
-    except TypeError:
-        recipes_data[i]['pieces'] = None
-
-print(recipes_data)
-
-csv_recipes_today = open(r'most_made_today_01.csv', 'w' ,newline='')
-csv_writer= csv.writer(csv_recipes_today)
-# headers = categories
-headers=recipes_data[0].keys()
-csv_writer.writerow(headers)
-for key,row in recipes_data.items():
-    csv_writer.writerow(row.values())
-csv_recipes_today.close()
+def write_data_to_csv(recipes_data):
+    """ Writing the 'recepies_data' to csv file """
+    with open(r'most_made_today_01.csv', 'w' ,newline='') as csv_recipes_today:
+        csv_writer= csv.writer(csv_recipes_today)
+        headers = CATEGORIES
+        csv_writer.writerow(headers)
+        for key,row in recipes_data.items():
+            csv_writer.writerow(row.values())
