e4ac8592cd493a07095e10b26f26c37d29f2fc17
Ravit <ravit2244@gmail.com>
2019-12-01 16:34:53 +0000

    config.py edited online with Bitbucket

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 # constants
+# associated with the module recipe_details.py
 RECIPE_DETAILS = ['sub_category', 'url', 'author', 'review', 'summary', 'name', 'rating', 'image', 'prep_time',
               'calories', 'pieces', 'ingredients', 'directions']
               
+# associated with the module scrapping.py     
 URL = 'https://www.allrecipes.com/'
 
 
 
 # temporary (will be removed in version 2)
+
+# associated with the module scrapping.py     
 CATEGORY = ['Cookies']
 SUBCATEGORY = ['Butter Cookies']
