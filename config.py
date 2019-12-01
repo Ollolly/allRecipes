8908247ef887c1d9862ceb074291687bd9574057
Ravit <ravit2244@gmail.com>
2019-12-01 16:29:24 +0000

    config.py created online with Bitbucket

---
+++ config.py
@@ -0,0 +1,11 @@
+# constants
+RECIPE_DETAILS = ['sub_category', 'url', 'author', 'review', 'summary', 'name', 'rating', 'image', 'prep_time',
+              'calories', 'pieces', 'ingredients', 'directions']
+              
+URL = 'https://www.allrecipes.com/'
+
+
+
+# temporary (will be removed in version 2)
+CATEGORY = ['Cookies']
+SUBCATEGORY = ['Butter Cookies']
