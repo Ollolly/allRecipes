e106f0998c5c2c86a00289347b1321dfea1d0d65
Ravit Mesika <ravit2244@gmail.com>
2019-12-04 19:51:23 +0200

    updating parse_arguments_advanced function

--- cli.py
+++ cli.py
@@ -3,42 +3,48 @@ import logging.config
 import argparse
 import scrapping as sc
 from config import URL, CATEGORY, LOG_CONF
+import sys
 
 
-def parse_arguments_advanced(categories):
+def parse_arguments_advanced():
 
-    categories_str = ', '. join(categories)
+    # categories_str = ', '. join(categories)
     parser = argparse.ArgumentParser(
         description="Script Description"
     )
-    parser.add_argument("--l", help="""
+    parser.add_argument("-l", "--list", help="""
                         output list of possible recipes categories if no further arguments is given
-                        or list of possible recipes sub-categories of a requested category if the latter is given after the flag
-                        for example: --r will output all optional categories and --r Cookies will output all optional subcategories associated with Cookies""")
+                        or list of possible recipes of sub-categories associated with the requested
+                        category if the -c flag is specified with the category name
+                        for example: '-l' will output list of optional categories.
+                        '-lc cat1' will output all optional subcategories associated 
+                        with the category cat1 """, action="store_true")
 
-    parser.add_argument("--g", help="""
-                        Stands for a "get" mode. scraping data .... and return a relevant data regarding the requested category and subcategoy
-                         recieves two arguments: category and sub-category 
-                         according to the mode given by --s flag.
-                        Note: 1. don't forget to add "" around category/subcategory of >=2 words.
-                        2. This flag has to be followed by the --h flag
-                    
-                    
-                        For example: --g Cookies "Butter Cookies"
-                        """)
+    parser.add_argument("-c", "--category", help="""receives one argument (category) after the
+                        -l flag and outputs the list of sub categories associated 
+                        with the specified category. for example: '-lc Cookies' will output the 
+                        following list: ['Butter Cookies', 'Bar Cookies', .....]
+                        """,#TODO add a condition that this flag can not be independent
+                        action="store")
 
-    parser.add_argument("--s", help="""
-                        Indicates the requested format to save the data, 
-                        choose "DB" to save the data in sql DB (divided to tables) or "csv" to save it as a whole in a csv file
-                        default is to save it as a sql DB 
-                        Valid categories are: {}""".format(categories_str)
-                        , choices=['DB', 'csv'])
-    # parser.add_argument("name", help="""
-    #                     Indicates the name of the user we want to greet
-    #                     """)
-    #
-    # parser.add_argument("--capitalize", help="Capitalizes the user's name",
-    #                 action="store_true")
+    parser.add_argument("-g","--get", help="""
+                        scraping data of the requested category and subcategory and save it in a 
+                        csv and sql data base. receives two arguments: category and sub-category 
+                        Note: don't forget to add "" around category/subcategory of >=2 words.
+                    
+                        For example: -g Cookies "Butter Cookies" -s csv will output the recipes 
+                        associated with Butter Cookies.
+                        """, nargs='+')
+
+
+    # parser.add_argument("-s","--save", help="""
+    #                     Indicates the requested format to save the data,
+    #                     recieves one argument 'DB' or 'csv'.
+    #                     choose "DB" to save the data in sql DB (divided to tables) or "csv" to save it in a csv file
+    #                     if -s is not specified, the data by default will be saved in a sql DB.
+    #                     """, required='--get' in sys.argv, choices=['DB', 'csv'])
+    # TODO - add s flag conditional in the future to enable the user choose how to save the data
+
     arguments = parser.parse_args()
     return arguments
 
@@ -47,29 +53,40 @@ def main():
     """ logger initialization """
     logging.config.fileConfig(LOG_CONF)
     logging.info('Scrapping category links')
-    categories = sc.get_valid_categories(URL)
-    args = parse_arguments_advanced(categories)
-    requested_category = args.category
-    link = sc.get_category_link(URL, requested_category)
-    logging.info('Scrapping subcategory links')
-    subcategory = sc.get_category_links(link, subcategories)
-    print(subcategory)
+    args = parse_arguments_advanced()
+    print(args)
+    # in case l is given alone
+    if args.list and args.category is None:
+        category_list = sc.get_category_list(URL)
+        print(category_list)
 
+    # in case -lc is given
+    if not(args.list is None) and not(args.category is None):
+        if len(arg.list)!=1:
+            cat = args.category
+            cat_link = sc.get_category_links(URL, cat)
+            sub_category_list = sc.get_category_list(cat_link[cat])
+            print(sub_category_list)
+        else:
+            sys.exit("invalid input")
 
+    # in case -g is given correctly(with category and aub-category)
+    if not (args.get is None):
+        if len(args.get)<=1:
+            sys.exit("invalid input")
+        else:
+            cat = args.get[0]
+            sub_cat = args.get[1:]
+            cat_link = sc.get_category_links(URL, cat)
+            sub_cat_links = sc.get_category_links(cat_link[cat], sub_cat)
+            recipes = {}
+            for cat, link in sub_cat_links.items():
+                recipes[cat] = sc.get_recipe_links(link)
 
-
-    requested_category = [CATEGORY[0]]
-    cat_link = sc.get_category_links(URL, requested_category)
-    logging.info('Scrapping subcategory links')
-    subcategory = sc.get_category_links(cat_link[requested_category[0]], SUBCATEGORY)
-
-    recipes = {}
-    for cat, link in subcategory.items():
-        recipes[cat] = sc.get_recipe_links(link)
-
-    # extract all data and write it to file
-    data = sc.scrap_data(requested_category, recipes)
-    sc.write_data_to_csv(data)
+            logging.debug(sub_cat_links)
+            data = sc.scrap_data(cat, recipes)
+            logging.debug(data)
+            sc.write_data_to_csv(data)
 
 
 if __name__ == '__main__':
