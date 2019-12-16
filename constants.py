7e4c2d984f6942bc303df6a5d3518c46770d2129
olga <hazin.olga@gmailcom>
2019-12-16 18:14:30 +0200

    PEP8

--- constants.py
+++ constants.py
@@ -14,23 +14,23 @@ ING_DETAILS = ['label', 'enerc_kcal', 'fat', 'procnt','chocdf','related_recipes'
 
 
 # measurments tool used in the module recipe_details.py
-MEASUREMENTS = ('cup','cupBars','cupcup','cupcups','cups','cupscup','cupscups','ounce','ouncescup',
-                'pinch','pound','ripe','tablespoon','tablespoons','teaspoon','teaspooncup',
-                'teaspooncups','teaspoons','teaspoonscup','teaspoonsounce','verycup')
+MEASUREMENTS = ('cup', 'cupBars', 'cupcup', 'cupcups', 'cups', 'cupscup', 'cupscups', 'ounce', 'ouncescup',
+                'pinch', 'pound', 'ripe', 'tablespoon', 'tablespoons', 'teaspoon', 'teaspooncup',
+                'teaspooncups', 'teaspoons', 'teaspoonscup', 'teaspoonsounce', 'verycup')
 
 
-MEASUREMENTS_DICT = {'cup': ['cup','cupBars','cupcup','cupcups','cups','cupscup','cupscups','ouncescup','verycup'],
-                     'ounce': ['ounce', 'pinch'], 'pound': ['pound'],
-                     'tablespoon': ['tablespoon','tablespoons'],
-                     'teaspoon': ['teaspoon','teaspooncup','teaspooncups','teaspoons','teaspoonscup','teaspoonsounce'],
-                     'item': []}
+MEASUREMENTS_DICT = {'cup': ['cup', 'cupBars', 'cupcup', 'cupcups', 'cups', 'cupscup', 'cupscups', 'ouncescup', 'verycup'],
+                     'ounce': ['ounce', 'ounces', 'pinch'], 'pound': ['pound'],
+                     'tablespoon': ['tablespoon', 'tablespoons'],
+                     'teaspoon': ['teaspoon', 'teaspooncup', 'teaspooncups', 'teaspoons', 'teaspoonscup', 'teaspoonsounce'],
+                     'item': ['item']}
 
 
 # measurments tool used in the module recipe_details.py
 INGREDIENTS = ('alfalfa', 'almond', 'almonds', 'anchovy', 'anise', 'apple', 'asparagus', 'avocado', 'bacon',
                'baking powder', 'balsamic', 'banana', 'basil', 'beans', 'beef', 'beer', 'beet', 'blackberry',
-               'blueberries', 'bonein', 'bourbon', 'bread', 'broccoli', 'broth', 'brussels', 'buckwheat', 'butter',
-               'buttermilk', 'canola', 'carrot', 'cayenne', 'celery', 'cereal', 'cheddar', 'cheese', 'cherries',
+               'blueberries', 'bonein', 'bourbon', 'bread', 'brandy', 'broccoli', 'broth', 'brussels', 'buckwheat',
+               'butter', 'buttermilk', 'canola', 'carrot', 'cayenne', 'celery', 'cereal', 'cheddar', 'cheese', 'cherries',
                'cherry', 'chicken', 'chickpeas', 'chips', 'chocolate', 'cider', 'cinnamon', 'cloves', 'cocoa',
                'coconut', 'condensed milk', 'corn', 'cornbread', 'cornstarch', 'cranberries', 'cranberry', 'cream',
                'crumbs', 'cucamber', 'cucumbers', 'curry', 'dijon', 'duck', 'egg', 'eggnog', 'farina', 'feta', 'fish',
